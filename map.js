62df84fc2416b881a4b372f1d5c9fd4466f805eb
jayzosayers <65043706+jayzosayers@users.noreply.github.com>
2020-10-10 16:45:03 +0000

    Add files via upload

--- map.js
+++ map.js
@@ -36,7 +36,6 @@ var icons = {
   }
 };
 
-
 function initialiseMap() {
 
   // Load data from an example Google spreadsheet that contains latitude and longitude columns using Google Sheets API v4 that returns JSON.
@@ -44,28 +43,28 @@ function initialiseMap() {
   // https://sheets.googleapis.com/v4/spreadsheets/ID_OF_YOUR_GOOGLE_SPREADSHEET/values/Sheet1!A2:Q?key=YOUR_API_KEY
   // Also make sure your API key is authorised to access Google Sheets API - you can enable that through your Google Developer console.
   // Finally, in the URL, fix the sheet name and the range that you are accessing from your spreadsheet. 'Sheet1' is the default name for the first sheet.
-  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1K-rWGh9SKkrkuGt1PSfU-G4HykE3zlvlIUGbB6zYIJc/values/'ROC Posts'!A2:M?key=AIzaSyAwhjy9-JjXUOIKjez_1auka6ThFfQEksY", function(data) {
-    	// data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
-    	// Modify the code below to suit the structure of your spreadsheet.
-    	$(data.values).each(function() {
-    		var location = {};
-				location.title = this[2];
-				location.latitude = parseFloat(this[0]);
-      	        		location.longitude = parseFloat(this[1]);
-                		location.group = this[3];
-       	        	location.condition = this[4];
-                		location.visited = this[6];
-                		location.visitdate = this[7];
-	  		    	locations.push(location);
-    	});
+  $.getJSON("https://sheets.googleapis.com/v4/spreadsheets/1K-rWGh9SKkrkuGt1PSfU-G4HykE3zlvlIUGbB6zYIJc/values/'ROC Posts'!A2:M?key=AIzaSyAwhjy9-JjXUOIKjez_1auka6ThFfQEksY", function (data) {
+    // data.values contains the array of rows from the spreadsheet. Each row is also an array of cell values.
+    // Modify the code below to suit the structure of your spreadsheet.
+    $(data.values).each(function () {
+      var location = {};
+      location.title = this[2];
+      location.latitude = parseFloat(this[0]);
+      location.longitude = parseFloat(this[1]);
+      location.group = this[3];
+      location.condition = this[4];
+      location.visited = this[6];
+      location.visitdate = this[7];
+      locations.push(location);
+    });
 
-      // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
-      var mapOptions = {
-        zoom: 10,
-        center: new google.maps.LatLng(0, 0)
-      };
-      var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      setLocations(map, locations);
+    // Center on (0, 0). Map center and zoom will reconfigure later (fitbounds method)
+    var mapOptions = {
+      zoom: 10,
+      center: new google.maps.LatLng(0, 0)
+    };
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    setLocations(map, locations);
   });
 }
 
@@ -91,38 +90,39 @@ function createMarker(map, location, infowindow) {
     lng: parseFloat(location.longitude)
   };
   var iconType = 'none';
-  
-  if (location.condition === undefined) {
-      iconType = 'none';
+  console.log(location.title)
+  if (location.condition == null || location.condition === undefined) {
+    iconType = 'none';
   } else {
-      iconType = location.condition;
-      iconType = iconType.replace(/\s/g, '');
+    iconType = location.condition;
+    iconType = iconType.replace(/\s/g, '');
   }
-  
+  console.log(iconType)
+
   var marker = new google.maps.Marker({
     position: position,
     icon: icons[iconType].icon,
     map: map,
     title: location.title,
   });
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent('<div>'+
-    '<p><strong>' + location.title + '</strong></p>' +
-    ((location.group === undefined) ? "" : ('<p><strong>Group: </strong>' + location.group + '</p>')) +
-    ((location.condition === undefined) ? "" : ('<p><strong>Condition: </strong>' + location.condition + '</p>')) +
-    ((location.visited === undefined) ? "" : ('<p><strong>Visited: </strong>' + location.visited + ((location.visited === 'NO') ? "" : (' (' + location.visitdate + ')')) + '</p>')) +
-    ((location.notes === undefined) ? "" : ('<p><strong>Notes: </strong>' + location.notes)) +
-    '</div>');
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent('<div>' +
+      ('<h6><span class="dataheader">' + location.title + '</span></h6>') +
+      ((location.group === undefined) ? "" : ('<p class="balloon"><span class="dataheader">Group: </span>' + location.group + '</p>')) +
+      ((location.condition === undefined) ? "" : ('<p class="balloon"><span class="dataheader">Condition: </span>' + location.condition + '</p>')) +
+      ((location.visited === undefined) ? "" : ('<p class="balloon"><span class="dataheader">Visited: </span>' + location.visited + ((location.visited === 'NO') ? "" : (' (' + location.visitdate + ')')) + '</p>')) +
+      ((location.notes === undefined) ? "" : ('<p class="balloon"><span class="dataheader">Notes: </span>' + location.notes)) +
+      '</div>');
     infowindow.open(map, marker);
   });
   google.maps.event.addListener(map, 'zoom_changed', function () {
     var maptype = map.getMapTypeId();
     if (map.getZoom() >= map.mapTypes[maptype].maxZoom) {
-        if (map.getMapTypeId() != google.maps.MapTypeId.HYBRID) {
-            map.setMapTypeId(google.maps.MapTypeId.HYBRID)
-            map.setTilt(0); // disable 45 degree imagery
-        }
+      if (map.getMapTypeId() != google.maps.MapTypeId.HYBRID) {
+        map.setMapTypeId(google.maps.MapTypeId.HYBRID)
+        map.setTilt(0); // disable 45 degree imagery
+      }
     }
-});;
+  });;
   return marker;
 }
